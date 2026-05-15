bb9be746adbe532606f90c1c82d6d663013bf3e6
jgbrandaooeiras-lab <jgbrandao.oeiras@gmail.com>
2026-05-15 20:42:06 -0300

    Create Estruturas condicionais.java

---
+++ Estruturas condicionais.java
@@ -0,0 +1,15 @@
+package com.meuprojeto;
+public class Projeto2 {
+//IF e ELSE de uma maneira mais simples com o operador ternario:
+	public static void main(String[] args) {		
+double salario = 6000;
+String MensagemDoar = "Eu vou doar 500 pro DevDojo";
+String MensagemNaoDoar = "Não tenho condições agora, mas vou ter!";
+// Doar se salario>5000
+String Resultado =  salario > 5000 ? "Eu vou doar 500 pro DevDojo" : "Não tenho condições agora, mas vou ter!";
+System.out.println(Resultado);
+	}
+
+}
+
+
